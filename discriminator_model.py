Fix backpropagation

discriminator_model.py
@@ -1,19 +1,11 @@
 #This is the discriminator model for the CycleGAN model
 import torch
 import torch.nn as nn
+from torchinfo import summary
 
 # Define the block for discriminator model
 class Block(nn.Module):
     # initialize the class with in_channels, out_channels, and stride
-
-    # def __init__(self, in_channels, out_channels, stride):
-    #     super(Block, self).__init__()
-    #     # convolutional layer
-    #     self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=stride, padding=1)
-    #     # batch normalization
-    #     self.batch_norm = nn.BatchNorm2d(out_channels)
-    #     # leaky relu
-    #     self.leaky_relu = nn.LeakyReLU(0.2, inplace=True)
 
     def __init__(self, in_channels, out_channels, stride):
         super().__init__()
@@ -68,6 +60,7 @@ def test():
     model = Discriminator(in_channels=1)
     preds = model(x)
     print(preds.shape)
+    summary(model, (1, 1, 512, 512))
 
 if __name__ == "__main__":
     test()

simulator_model.py
@@ -1,3 +1,5 @@
+# Maybe rewrite this as a function so the forward pass is clear, the module seems to break back propagation
+
 import numpy as np
 # import modules.grid
 from itertools import permutations
@@ -5,64 +7,69 @@ import cv2
 from PIL import Image
 import torch
 import scipy.io # for loading .mat file
+from torchvision import transforms
+import torch.nn as nn
 
-
-class Simulator:
-
-    def __init__(self, phoscoding, grid, imgsize = 256):
-        self.phoscoding = phoscoding # this is the phosphene coding (level of brightness), output from phosphene_encoder.py
+class Simulator(nn.Module):
+    def __init__(self, grid, device, imgsize = 256):
+        super().__init__()
+        # self.phoscoding = phoscoding # this is the phosphene coding (level of brightness), output from phosphene_encoder.py
         self.grid = grid * 512/imgsize  # this should be a phosphene map, output from matlab, resize the coordinates to the size of the image (512*512)
+        self.device = device
         self.size = 512
-        self.image = self.phos_img()
 
-
-    def phos_img(self):
-        img = np.zeros((self.size, self.size))
-        n = len(self.phoscoding)
-        columnsInImage, rowsInImage = np.meshgrid(np.arange(1, self.size + 1), np.arange(1, self.size + 1))
+    def phos_img(self, phoscoding):
+        img = torch.zeros(1, 1, self.size, self.size, device=self.device)
+        n = len(phoscoding)
+        columnsInImage, rowsInImage = torch.meshgrid(torch.arange(1, self.size + 1, device=self.device), torch.arange(1, self.size + 1, device=self.device))
         x = self.grid[:, 0] # x coordinate of phosphene in pixel
         y = self.grid[:, 1] # y coordinate of phosphene in pixel
         radii = self.grid[:, 2] # radius of phosphene in pixel
 
         for i in range(n):
-            if self.phoscoding[i] > 0.5:
-                phosphene = self.render_phosphene(x[i], y[i], radii[i], rowsInImage, columnsInImage) * 1
-                gau_phosphene = cv2.GaussianBlur(np.float32(phosphene), ksize=(0, 0), sigmaX=radii[i] / 3, borderType=cv2.BORDER_REPLICATE)
-                gau_phosphene[gau_phosphene < 0.05] = 0
-                img = img + gau_phosphene
+            brightness = phoscoding[i]
+            phosphene = self.render_phosphene(y[i], x[i], radii[i], rowsInImage, columnsInImage) * brightness
+            phosphene = phosphene[None, None]
+            # Set parameters for Gaussian blur
+            sigma = float(radii[i] / 3)
+            kernel_size = int(2 * np.ceil(2 * sigma) + 1)
+            # Apply Gaussian blur
+            gau_phosphene = transforms.GaussianBlur(kernel_size=kernel_size, sigma=sigma)(phosphene)
+
+            gau_phosphene[gau_phosphene < 0.05] = 0
+            img += gau_phosphene
 
         img[img > 1] = 1
-        if np.max(img) != 0: # normalization, otherwise imange will be white and show all white
-            img = img / np.max(img)
+        if torch.max(img) != 0:  # normalization, otherwise the image will be white and show all white
+            img = img / torch.max(img)
 
         return img
 
-    def render_phosphene(self, x, y, radius, rowsInImage, columnsInImage):
-        phosphene = (rowsInImage - y) ** 2 + (columnsInImage - x) ** 2 <= radius ** 2
+    def render_phosphene(self, y, x, radius, rowsInImage, columnsInImage):
+        phosphene = torch.square(rowsInImage - x) + torch.square(columnsInImage - y) <= radius ** 2
         return phosphene
 
+    def forward(self, phoscoding):
+        return self.phos_img(phoscoding)
 
+# convert everything to pytorch
+# Set the device to either 'cuda' for GPU or 'cpu' for CPU
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 def test():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     phosnum = 1200
-    phoscoding = np.random.rand(phosnum)
-    # phoscoding = np.random.randint(2, size=phosnum)
-    # grid = np.random.rand(phosnum, 3)    # x, y, radius
-    # grid[:, 0] *=100
-    # grid[:, 0] = grid[:, 0] + 64
-    # grid[:, 1] *=100
-    # grid[:, 1] = grid[:, 1] + 64
-    # grid[:, 2] *=3
+    phoscoding = torch.ones(phosnum, device=device)
     phos_map_mat = scipy.io.loadmat('data/grids/100610_12ea_UEA_pm_pix.mat')
     grid = phos_map_mat['grid']
-    simu = Simulator(phoscoding, grid/1080*512, imgsize=512)
-    img = simu.image
+    simu = Simulator(grid/1080*512, device, imgsize=512)
+    img = simu(phoscoding)
 
     if img is None:
         print("Check file path")
     else:
         # cv2.imshow("Display", img)
         # cv2.waitKey()
-        formatted = (img * 255 / np.max(img)).astype('uint8')
+        formatted = (img * 255 / torch.max(img)).byte().numpy().squeeze()
         img = Image.fromarray(formatted)
         img.show()
 

train.py
@@ -25,7 +25,7 @@ import scipy.io # for loading .mat file
 # zebra -> target/phosphene
 
 def train_fn(
-    disc_H, disc_Z, gen_Z, gen_H, loader, opt_disc, opt_gen, l1, mse, d_scaler, g_scaler, grid
+    disc_H, disc_Z, gen_Z, gen_H, simu, loader, opt_disc, opt_gen, l1, mse, d_scaler, g_scaler, grid
 ):
     H_reals = 0 # H_real is the output of discriminator for real horse image
     H_fakes = 0 # H_fake is the output of discriminator for fake horse image
@@ -39,7 +39,7 @@ def train_fn(
         with torch.cuda.amp.autocast():
             fake_horse = gen_H(zebra)
             D_H_real = disc_H(horse)
-            D_H_fake = disc_H(fake_horse.detach())
+            D_H_fake = disc_H(fake_horse)
             H_reals += D_H_real.mean().item()
             H_fakes += D_H_fake.mean().item()
             D_H_real_loss = mse(D_H_real, torch.ones_like(D_H_real))
@@ -47,12 +47,15 @@ def train_fn(
             D_H_loss = D_H_real_loss + D_H_fake_loss
 
             fake_zebra_endcoding = gen_Z(horse).to(config.DEVICE)
-            simu = Simulator(torch.sigmoid(fake_zebra_endcoding), grid, imgsize=512)
-            fake_zebra = torch.from_numpy(simu.image).to(config.DEVICE)
-            fake_zebra = fake_zebra[None, None].float() # add batch and channel dimension
+            # simu = Simulator(imgsize=512)
+            # fake_zebra = simu(torch.sigmoid(fake_zebra_endcoding), grid, config.DEVICE)
+
+            # simu = Simulator(grid / 1080 * 512, config.DEVICE, imgsize=512)
+            fake_zebra = simu(torch.sigmoid(fake_zebra_endcoding))
+            # fake_zebra = fake_zebra[None, None] # add batch and channel dimension
 
             D_Z_real = disc_Z(zebra)
-            D_Z_fake = disc_Z(fake_zebra.detach())
+            D_Z_fake = disc_Z(fake_zebra)
             D_Z_real_loss = mse(D_Z_real, torch.ones_like(D_Z_real))
             D_Z_fake_loss = mse(D_Z_fake, torch.zeros_like(D_Z_fake))
             D_Z_loss = D_Z_real_loss + D_Z_fake_loss
@@ -61,7 +64,7 @@ def train_fn(
             D_loss = (D_H_loss + D_Z_loss) / 2
 
         opt_disc.zero_grad()
-        d_scaler.scale(D_loss).backward()
+        d_scaler.scale(D_loss).backward(retain_graph=True)
         d_scaler.step(opt_disc)
         d_scaler.update()
 
@@ -75,9 +78,12 @@ def train_fn(
 
             # cycle loss
             cycle_zebra_encoding = gen_Z(fake_horse).to(config.DEVICE)
-            simu_cyc = Simulator(torch.sigmoid(cycle_zebra_encoding), grid, imgsize=512)
-            cycle_zebra = torch.from_numpy(simu_cyc.image).to(config.DEVICE)
-            cycle_zebra = cycle_zebra[None, None].float()
+            # simu_cyc = Simulator(imgsize=512)
+            # cycle_zebra = simu_cyc(torch.sigmoid(cycle_zebra_encoding), grid, config.DEVICE)
+
+            # simu = Simulator(grid / 1080 * 512, config.DEVICE, imgsize=512)
+            cycle_zebra = simu(torch.sigmoid(cycle_zebra_encoding))
+            # cycle_zebra = cycle_zebra[None, None]
 
             cycle_horse = gen_H(fake_zebra)
             cycle_zebra_loss = l1(zebra, cycle_zebra)
@@ -102,7 +108,7 @@ def train_fn(
             # zebra_pix_sample = horse[:, 0, x, y].to(config.DEVICE)
             # zebra_pix_sample = torch.squeeze(zebra_pix_sample)
             # zebra_pix_sample = (zebra_pix_sample-min(zebra_pix_sample))/(max(zebra_pix_sample)-min(zebra_pix_sample))
-            # BCE_loss_zebar = criterion(fake_zebra_endcoding, zebra_pix_sample)
+            # BCE_loss_zebra = criterion(fake_zebra_endcoding, zebra_pix_sample)
 
             # cycle_zebra_pix_sample = cycle_horse[:, 0, x, y].to(config.DEVICE)
             # cycle_zebra_pix_sample = torch.squeeze(cycle_zebra_pix_sample)
@@ -115,7 +121,7 @@ def train_fn(
             G_loss = (
                 loss_G_Z
                 + loss_G_H
-                # + BCE_loss_zebar
+                # + BCE_loss_zebra
                 # + BCE_loss_cycle
                 + cycle_zebra_loss * config.LAMBDA_CYCLE
                 + cycle_horse_loss * config.LAMBDA_CYCLE
@@ -124,15 +130,15 @@ def train_fn(
             )
 
         opt_gen.zero_grad()
-        g_scaler.scale(G_loss).backward()
+        g_scaler.scale(G_loss).backward(retain_graph=True)
         g_scaler.step(opt_gen)
         g_scaler.update()
 
         if (idx+1) % 200 == 0:
             save_image(fake_horse, f"saved_images/g_original_{idx}.png") # original image
             save_image(fake_zebra, f"saved_images/g_phosphene_{idx}.png") # phosphene image
-            save_image(zebra, f"saved_images/phosphene_{idx}.png") # phosphene image
-            save_image(horse, f"saved_images/cycle_{idx}.png") # cycle image
+            # save_image(zebra, f"saved_images/phosphene_{idx}.png") # phosphene image
+            # save_image(horse, f"saved_images/cycle_{idx}.png") # cycle image
 
         loop.set_postfix(H_real=H_reals / (idx + 1), H_fake=H_fakes / (idx + 1))
 
@@ -145,6 +151,7 @@ def main():
     disc_Z = Discriminator(in_channels=1).to(config.DEVICE) # classify image of zebras
     gen_Z = Phoscoder(img_channels=1, num_residuals=4).to(config.DEVICE) # generate phosphene image (zebra) from orignial (horse)
     gen_H = Generator(img_channels=1, num_residuals=4).to(config.DEVICE) # generate image of horse from zebra
+    simu = Simulator(grid / 1080 * 512, config.DEVICE, imgsize=512) # simulate phosphene image from phosphene encoding
     opt_disc = optim.Adam(
         list(disc_H.parameters()) + list(disc_Z.parameters()),
         lr=config.LEARNING_RATE,
@@ -218,6 +225,7 @@ def main():
             disc_Z,
             gen_Z,
             gen_H,
+            simu,
             loader,
             opt_disc,
             opt_gen,